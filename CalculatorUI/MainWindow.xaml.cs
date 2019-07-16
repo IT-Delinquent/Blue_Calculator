b52709b7bcf258108b4823160580e2362483de34
IT-Delinquent <48588045+IT-Delinquent@users.noreply.github.com>
2019-07-16 16:13:35 +0100

    Fully working product :)

--- CalculatorUI/MainWindow.xaml.cs
+++ CalculatorUI/MainWindow.xaml.cs
@@ -21,6 +21,24 @@ namespace CalculatorUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Creating initial variables and hashtable
+        double FirstNumber;
+        string Operation;
+        readonly Hashtable numbers = new Hashtable()
+        {
+            {"Zero", "0" },
+            {"One", "1" },
+            {"Two", "2" },
+            {"Three", "3" },
+            {"Four", "4" },
+            {"Five", "5" },
+            {"Six", "6" },
+            {"Seven", "7" },
+            {"Eight", "8" },
+            {"Nine", "9" }
+        };
+
+        //Initializing component
         public MainWindow()
         {
             InitializeComponent();
@@ -53,44 +71,68 @@ namespace CalculatorUI
             }
         }
 
-        double FirstNumber;
-        string Operation;
-        readonly Hashtable numbers = new Hashtable()
+        //Update Cal TextBox with the option to override or append
+        void UpdateCalText(string input, bool append)
         {
-            {"Zero", "0" },
-            {"One", "1" },
-            {"Two", "2" },
-            {"Three", "3" },
-            {"Four", "4" },
-            {"Five", "5" },
-            {"Six", "6" },
-            {"Seven", "7" },
-            {"Eight", "8" },
-            {"Nine", "9" }
-        };
-
-        private void Num_Click(object sender, RoutedEventArgs e)
-        {
-            Button srcButton = e.Source as Button;
-            string currentValue = numbers[srcButton.Name].ToString();
-            if (Cal.Text == "0" && Cal.Text != null )
+            if (append)
             {
-                Cal.Text = currentValue;
+                Cal.Text += input;
             }
             else
             {
-                Cal.Text += currentValue;
+                Cal.Text = input;
             }
-            Op.Text += currentValue;
         }
 
+        //Update Op TextBox with the option to override or append
+        void updateOpText(string input, bool append)
+        {
+            if (append)
+            {
+                Op.Text += input;
+            }
+            else
+            {
+                Op.Text = input;
+            }
+        }
+
+        //Returns the current value of the Cal TextBox
+        string GetCalText()
+        {
+            return Cal.Text;
+        }
+
+        //Event for when the user presses a number button
+        private void Num_Click(object sender, RoutedEventArgs e)
+        {
+            //Getting the source button by name
+            Button srcButton = e.Source as Button;
+            string currentValue = numbers[srcButton.Name].ToString();
+            //Checking if the Cal TextBox contains just a "0" or if it is null
+            if (GetCalText() == "0" && GetCalText() != null )
+            {
+                UpdateCalText(currentValue, false);
+            }
+            else
+            {
+                UpdateCalText(currentValue, true);
+            }
+            updateOpText(currentValue, true);
+        }
+
+        //Event for when the user presses an operator button
         private void Operator_Click(object sender, RoutedEventArgs e)
         {
+            //Checking if the Cal TextBox text can be parsed into a type double
+            //If not, then do nothing
             if (double.TryParse(Cal.Text, out double i))
             {
+                //Setting first number, updating Cal TextBox and getting source button
                 FirstNumber = Convert.ToDouble(i);
-                Cal.Text = "0";
+                UpdateCalText("0", false);
                 Button srcButton = e.Source as Button;
+                //Using switch to update the operation variable based on the source button's name
                 switch (srcButton.Name)
                 {
                     case "Plus":
@@ -106,69 +148,74 @@ namespace CalculatorUI
                         Operation = "*";
                         break;
                 }
-                Op.Text += Operation;
+                //Updating Op TextBox
+                updateOpText(Operation, true);
             }
         }
 
-
+        //Event for when the user clicks the equals button
         private void Equals_Click(object sender, RoutedEventArgs e)
         {
+            //Creating two new variables for the calculation
             double SecondNumber, Result;
             SecondNumber = Convert.ToDouble(Cal.Text);
-
+            //Switching the Operator variable to see what to do
             switch (Operation)
             {
                 case "+":
                     Result = (FirstNumber + SecondNumber);
-                    Cal.Text = Convert.ToString(Result);
+                    UpdateCalText(Convert.ToString(Result), false);
                     FirstNumber = Result;
-                    Op.Text = Convert.ToString(Result);
+                    updateOpText(Convert.ToString(Result), false);
                     break;
                 case "-":
                     Result = (FirstNumber - SecondNumber);
-                    Cal.Text = Convert.ToString(Result);
+                    UpdateCalText(Convert.ToString(Result), false);
                     FirstNumber = Result;
-                    Op.Text = Convert.ToString(Result);
+                    UpdateCalText(Convert.ToString(Result), false);
                     break;
                 case "*":
                     Result = (FirstNumber * SecondNumber);
-                    Cal.Text = Convert.ToString(Result);
+                    UpdateCalText(Convert.ToString(Result), false);
                     FirstNumber = Result;
-                    Op.Text = Convert.ToString(Result);
+                    UpdateCalText(Convert.ToString(Result), false);
                     break;
                 case "/":
                     if (SecondNumber == 0)
                     {
-                        Cal.Text = "Cannot divide by 0!";
-                        Op.Text = string.Empty;
+                        UpdateCalText("Cannot divide by o!", false);
+                        updateOpText(string.Empty, false);
                         ToggleButtonExceptAC(false);
                     }
                     else
                     {
                         Result = (FirstNumber / SecondNumber);
-                        Cal.Text = Convert.ToString(Result);
+                        UpdateCalText(Convert.ToString(Result), false);
                         FirstNumber = Result;
-                        Op.Text = Convert.ToString(Result);
+                        UpdateCalText(Convert.ToString(Result), false);
                     }
                     break;
             }
         }
 
+        //Event for when the user presses the dot button
         private void Dot_Click(object sender, RoutedEventArgs e)
         {
+            //Checking if there is already a full stop in the Cal TextBox
             //Ensures their is only one . in a calculation
-            if (!Cal.Text.Contains(".") )
+            if (GetCalText().Contains("."))
             {
-                Cal.Text += ".";
-
+                UpdateCalText(".", true);
             }
         }
 
+        //Event for when the user presses the dot button
         private void AC_Click(object sender, RoutedEventArgs e)
         {
-            Cal.Text = "0";
-            Op.Text = string.Empty;
+            UpdateCalText("0", false);
+            updateOpText(string.Empty, false);
             ToggleButtonExceptAC(true);
+            Operation = string.Empty;
         }
     }
 }
